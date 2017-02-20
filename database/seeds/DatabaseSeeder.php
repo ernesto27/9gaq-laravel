8f9cd5b111ad659692e3cfc6f72f554b9d2f5f5a
ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-02-20 18:01:53 -0300

    Agrego migraciones - seeders de tabla categorías

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(PostTableSeeder::class);
-    	$this->call(CommentsTableSeeder::class);
-
+        $this->call(CommentsTableSeeder::class);
+    	$this->call(CategoryTableSeeder::class);
     }
 }
