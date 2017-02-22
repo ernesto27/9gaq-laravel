bee6c499b847d7f825b56ae618b300fbb9fb1ab7
ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-02-22 19:02:50 -0300

    Fix en migraciones de categorias

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
     public $timestamps = false;
+
+    public $table = 'categories';
 }
--- database/migrations/2017_02_08_203930_create_categories_table.php
+++ database/migrations/2017_02_08_203930_create_categories_table.php
@@ -27,8 +27,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::table('categories', function (Blueprint $table) {
-            Schema::dropIfExists('categories');
-        });
+         Schema::dropIfExists('categories');
     }
 }
